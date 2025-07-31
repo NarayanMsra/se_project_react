4a57ccb24eb36f0bbd9063a18ac9ebe9ff0a8034
Narayan Msra <arayannmsra@MacBook-Pro.local>
2025-07-30 20:23:05 -0400

    project complete

---
+++ vite-project/src/components/Header/Header.jsx
@@ -0,0 +1,35 @@
+import "./Header.css";
+import logo from "../../assets/logo.svg";
+import avatar from "../../assets/Avatar.png";
+
+function Header({ handleAddClick, weatherData }) {
+  const currentDate = new Date().toLocaleString("default", {
+    month: "long",
+    day: "numeric",
+    // year: "numeric",
+  });
+  return (
+    <header className="header">
+      <img className="header__logo" src={logo} alt="logo" />
+      <p className="header__date-and-location">
+        {currentDate}, {weatherData.city}
+      </p>
+      <button
+        onClick={handleAddClick}
+        type="button"
+        className="header__add-clothes-btn"
+      >
+        +Add clothes
+      </button>
+      <div className="header__user-container">
+        <p className="header__userName">Terrence Tegegn</p>
+        <img
+          src={avatar}
+          className="header__userAvatar"
+          alt="Terrence Tegegn"
+        />
+      </div>
+    </header>
+  );
+}
+export default Header;
---
+++ vite-project/src/components/ModalWithForm/ModalWithForm.jsx
@@ -0,0 +1,26 @@
+import "./ModalWithForm.css";
+import closebutton from "../../assets/closebutton.svg";
+import { useEffect } from "react";
+
+function ModalWithForm({ children, title, buttonText, activeModal, onClose }) {
+  return (
+    <div
+      className={`modal ${activeModal === "add-garment" && "modal__opened"}`}
+    >
+      <div className="modal__content">
+        <h2 className="modal__title">{title}</h2>
+        <button onClick={onClose} type="button" className="modal__close">
+          <img src={closebutton} alt="close icon" />
+        </button>
+        <form className="modal__form">
+          {children}
+          <button type="submit" className="modal__submit">
+            {buttonText}
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default ModalWithForm;
